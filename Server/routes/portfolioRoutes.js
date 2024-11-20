aa7ac155df83846fe3023bc1050635b8b7f0b3c8
nivswisa11 <swisaniv@gmail.com>
2024-11-20 19:17:06 +0200

    show crypto values across entire graph

--- Server/routes/portfolioRoutes.js
+++ Server/routes/portfolioRoutes.js
@@ -6,7 +6,7 @@ const {
     addPortfolio,
     deletePortfolio,
     calculateAnnualTax,
-    calculatePortfolioTax
+    calculatePortfolioNetGain
 } = require('../controllers/portfolioController');
 const { authMiddleware } = require('../middleware/authMiddleware');
 const router = express.Router();
@@ -17,7 +17,7 @@ router.get('/portfolio/recommendation/:symbol', getRecommendation);
 router.post('/portfolios', authMiddleware, addPortfolio);
 router.delete('/portfolios/:id', authMiddleware, deletePortfolio);
 router.get('/portfolio/annual-tax', authMiddleware, calculateAnnualTax);
-router.get('/portfolio/annual-tax/:portfolioId', authMiddleware, calculatePortfolioTax);
+router.get('/portfolio/annual-tax/:portfolioId', authMiddleware, calculatePortfolioNetGain);
 
 
 module.exports = router;
