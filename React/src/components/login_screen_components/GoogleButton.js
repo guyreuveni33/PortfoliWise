25b6ba901be46f59174e31e070e4d7af779a857a
guyreuveni33 <guyhaim55@gmail.com>
2024-12-17 17:50:00 +0200

    handle google auth redirection for production

--- React/src/components/login_screen_components/GoogleButton.js
+++ React/src/components/login_screen_components/GoogleButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from '../../styleMenu/Login.module.css';
 const API_URL = process.env.REACT_APP_API_URL; // Fetch the base URL from the .env file
 
-const GoogleButton = () => {
+const GoogleButton  = () => {
     const handleGoogleLogin = () => {
         window.location.href = `${API_URL}/api/users/google`; // We use href because we just want to trigger the flow
     };
