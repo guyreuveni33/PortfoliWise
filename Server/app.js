1a7622ff223dcc5ad017176549a8d662b770df1c
guyreuveni33 <guyhaim55@gmail.com>
2024-12-17 18:18:56 +0200

    handle google auth redirection for production

--- Server/app.js
+++ Server/app.js
@@ -17,20 +17,18 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 3001; // Use Railway's environment PORT
 
-const corsOptions = {
-    origin: [
-        'http://localhost:3000', // Local Development
-        'https://portfoli-wise.vercel.app', // Vercel Deployment
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // Allow cookies/auth headers
-};
+const allowedOrigins = [
+    'http://localhost:3000', // Local Development
+    'https://portfoli-wise.vercel.app', // Vercel frontend
+];
+
+app.use(cors({
+    origin: allowedOrigins, // Allow these origins
+    credentials: true,      // Allow cookies/auth headers if needed
+}));
+app.options('*', cors());
 
 
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // Explicitly handle preflight OPTIONS
 app.use(bodyParser.json());
 
 app.use(session({ secret: process.env.JWT_SECRET, resave: false, saveUninitialized: false }));
